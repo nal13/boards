0bae469a49c30515e8befaed95ae4e99279e357a
Nuno Gouveia <nunoalexgouv@hotmail.com>
2018-11-29 22:07:56 +0000

    Models. Migrations. First view. Templates. Tests. Static files.

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
+
 def home(request):
-    return HttpResponse('Heelo World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards':boards})
