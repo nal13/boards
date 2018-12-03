68a7be4012b92e03d8978d1b207b42d1f625da63
Nuno Gouveia <nunoalexgouv@hotmail.com>
2018-12-03 19:11:32 +0000

    Authentication.

--- mp/settings.py
+++ mp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
 
     'boards',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
